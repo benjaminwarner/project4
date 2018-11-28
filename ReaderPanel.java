f0442ca9d9d69d02d03d741aecba714e4b509f65
Benjamin Warner <benjamin.warner@onsemi.com>
2018-11-27 17:42:18 -0700

    Add border to ReaderPanel.java.

--- ReaderPanel.java
+++ ReaderPanel.java
@@ -1,11 +1,14 @@
-import java.awt.Dimension;
-
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class ReaderPanel extends JPanel {
 	
 	public ReaderPanel(int width, int height) {
 		setPreferredSize(new Dimension(width, height));
+		Border titledBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK), "Reader");
+		Border border = BorderFactory.createCompoundBorder(titledBorder, new EmptyBorder(10, 5, 10, 5));
+		setBorder(border);
 	}
 
 }
